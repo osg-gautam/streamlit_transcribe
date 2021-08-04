9f83a42b180b610ac9f607ad297a5a5fc61244a4
osg-gautam <84559475+osg-gautam@users.noreply.github.com>
2021-08-04 14:02:32 +0530

    Debugging to load model en_core_sm

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ import time
 analyzer = SentimentIntensityAnalyzer()
 from gensim.summarization.summarizer import summarize
 from gensim.summarization import keywords
-import en_core_web_sm
 
 import spacy
 nlp = spacy.load('en_core_web_sm')
